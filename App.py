4f1d895bf92b9bccd2cecefc2c2a30f49d7e6097
Szymon Rodziewicz <szymonrodant@gmail.com>
2022-12-03 18:13:00 +0100

    Initial commit

---
+++ App.py
@@ -0,0 +1,19 @@
+from google.cloud import bigquery
+
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+query = """
+    SELECT name, SUM(number) as total_people
+    FROM `bigquery-public-data.usa_names.usa_1910_2013`
+    WHERE state = 'TX'
+    GROUP BY name, state
+    ORDER BY total_people DESC
+    LIMIT 20
+"""
+query_job = client.query(query)  # Make an API request.
+
+print("The query data:")
+for row in query_job:
+    # Row values can be accessed by field name or index.
+    print("name={}, count={}".format(row[0], row["total_people"]))
